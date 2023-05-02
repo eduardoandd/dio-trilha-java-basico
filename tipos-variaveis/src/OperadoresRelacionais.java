b9414182a7a8c3d3f6afdecb0de2e74ba12daaab
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-05-02 12:02:21 -0300

    Operadores relacionais

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,42 @@
+
+
+public class OperadoresRelacionais {
+
+    //Operadores relacionais avalaiam a relação entre as variáveis
+
+    // == Quando desejamos verircar se uma variável é igual a outra.
+    // != Quando desejamos verificar se uma variáveç é diferente da outra
+    // > Quando desejamos verificar se uma variável é MAIOR que a outra
+    // < Quando desejamos verificar se uma variável é MENOR que a outra
+    // >= Quando desejamos verificar se uma variável é MAIOR que a outra
+    // >= Quando desejamos verificar se uma variável é MAIOR ou IGUAL que a outra
+    // <= Quando desejamos verificar se uma variável é MENOR ou IGUAL que a outra
+    
+    public static void main(String[] args) {
+        int n1,n2;
+        n1=1;
+        n2=2;
+
+        boolean simNao = n1 == n2;
+        System.out.println(simNao);
+
+        simNao = n1 < n2;
+        System.out.println(simNao);
+
+        simNao = n1 != n2;
+        System.out.println(simNao);
+
+        simNao = n1 <= n2;
+        System.out.println(simNao);
+
+       
+
+
+
+    }
+
+    
+    
+
+
+}
