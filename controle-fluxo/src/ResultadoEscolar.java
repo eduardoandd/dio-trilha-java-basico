049ef37ca67b35d1cb47a32ff778836b5b11e246
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-05-04 00:18:15 -0300

    CONDIÇÃO ENCADEADA

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,13 @@
 public class ResultadoEscolar{
 
     public static void main(String[] args) {
-        int nota =6;
+        int note =4;
 
-        if(nota >= 7){
+        if(note >= 7){
             System.out.println("APROVADO");
+        }
+        else if(note >= 5 && note <7){
+            System.out.println("RECUPERAÇÃO");
         }
         else{
             System.out.println("Reprovado");
