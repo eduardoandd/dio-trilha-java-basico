af2af70aa5b38323b63a019e4b2f24bbc6018eb1
eduardoandd <eduardo.andrade.dev@gmail.com>
2023-05-05 14:59:04 -0300

    Excessão personalizada

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura= scanner.nextDouble();
+
+        } 
+        catch (InputMismatchException e) {
+            System.out.println("Campo idade e altura precisa ser numérico");
+        } 
+    }
+}
